3dd36a3b98c2a016deb1a4ddd77a59cef6f2d430
Alberto Acerbi <alberto.acerbi@gmail.com>
2019-06-12 14:28:55 +0100

    Test of the effect of genetic propensity

--- test.R
+++ test.R
@@ -48,7 +48,14 @@ set_oranzees_environment <- function() {
   output
 }
 
+##
+##
+##
+##
+# TEST FOR ONE RUN:
+
 test_oranzees1 <- function(t_max){
+
   oranzees_environment <- set_oranzees_environment()
   test_environment <- oranzees_environment %>%
     filter(population == 'Uossob')
@@ -97,6 +104,7 @@ test_oranzees1 <- function(t_max){
   output
 }
 
+t_max = 12000
 my_test <- test_oranzees1(t_max)
 # matplot(my_test[,1:4], type = "l")
 
@@ -113,4 +121,71 @@ ggplot(data = data_to_plot) +
   facet_wrap(~category) +
   theme_bw() +
   theme(legend.position = "none")
+
+##
+##
+##
+##
+# TEST FOR MULTIPLE RUNS:
   
+test_oranzees2 <- function(t_max){
+  
+  N <- 100
+  pop <- matrix(c(rep(0, 38 * N), sample(1:300, N, replace = TRUE)), nrow = N, byrow = FALSE)
+  
+  for (t in 1:t_max) {
+    
+    # demographic bit:
+    pop[, 39] <- pop[, 39] + 1
+    pop[pop[, 39] >= 720, ] <- 0
+    old <- which(pop[, 39] > 300)
+    dead <- sample(c(TRUE, FALSE), length(old), prob = c(.01, .99), replace = TRUE)
+    pop[old[dead], ] <- 0
+    # innovation bit:
+    for(i in 1:N){
+      state <- ((sum(pop[i, 1:4]) >= 1) + (sum(pop[i, 5:8]) >= 1) + (sum(pop[i, 9:12]) >= 1) + (sum(pop[i, 13:16]) >= 1)) / 4
+      p_state <- rnorm(1, mean = 1 - state, sd = 0.05)
+      if(runif(1) < p_state){
+        p_peering <- rnorm(16, mean = colSums(pop[, 1:16]))
+        p_peering[p_peering < 0] = 0
+        innovation_i <- sample(1:16, 1, prob = p_peering)
+        if( runif(1) < test_environment$p_g[innovation_i]){
+          if(innovation_i <= 4){
+            pop[i, 1:4] = 0
+            pop[i, innovation_i] = 1
+          } else if(innovation_i > 4 & innovation_i <= 8){
+            pop[i, 5:8] = 0
+            pop[i, innovation_i] = 1
+          } else if(innovation_i > 8 & innovation_i <= 12){
+            pop[i, 9:12] = 0
+            pop[i, innovation_i] = 1
+          } else{
+            pop[i, 13:16] = 0
+            pop[i, innovation_i] = 1
+          }
+        }
+      }
+    }
+  }
+  output <- colSums(pop[, 1:16])
+}
+
+
+oranzees_environment <- set_oranzees_environment()
+test_environment <- oranzees_environment %>%
+  filter(population == 'Uossob')
+
+n_run <- 10
+t_max <- 12000
+gene_test1 <- matrix(nrow = n_run, ncol = 16)
+for(run in 1:n_run){
+  gene_test1[run,] <- test_oranzees2(t_max)
+}
+
+write(t(gene_test1), file = "output/gene_test1.csv", ncolumns = 16)
+write(test_environment$p_g[1:16], file = "output/gene_test1_p_g.csv", ncolumns = 1)
+
+
+
+
+
