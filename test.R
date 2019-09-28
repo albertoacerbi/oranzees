ef0eca3df89541cee0c69bf62b345be69a80fcae
Alberto Acerbi <alberto.acerbi@gmail.com>
2019-09-28 14:19:34 +0100

    adjusted figures 2 and 3

--- test.R
+++ test.R
@@ -120,16 +120,39 @@ analyse_patterns <- function(my_test){
 # write_csv(output, "output_test/test_many.csv")
 
 
+########################################################
+########################################################
+
+# SIMS FOR MS:
+
+# FIGURE 1
+alpha_e_test = seq(0.5, 1, by = 0.1)
+alpha_g_test = seq(0, 0.5, by = 0.1)
+
+output <- tibble(alpha_e = rep(alpha_e_test, each = 120), 
+                 alpha_g = rep(rep(alpha_g_test, each = 20), 6),
+                 n = NA)
+
+for(e in alpha_e_test){
+  for(g in alpha_g_test) {
+    test <- run_oranzees(t_max = 6000, alpha_e = e, alpha_g = g, S = 1, init_world = TRUE, n_run = 20)
+    out <- analyse_patterns(test)
+    output[output$alpha_e == e & output$alpha_g == g, ]$n <- out$n[61:80]
+    print(e)
+  }
+}
+write_csv(output, "sims_for_ms/data/figure_1.csv")
+
 # TWO POSSIBLE VISUALISATIONS:
 # MATRIX:
 test_many <- read_csv("output_test/test_many.csv") %>%
   group_by(alpha_g, alpha_e) %>%
   summarise( cultural = mean(n))
 ggplot(data=test_many) +
-    geom_raster(aes(x = alpha_g, y = alpha_e, fill = cultural)) +
-    scale_fill_gradientn(colors=c("red","white","blue")) +
-    geom_text(aes(x = alpha_g, y = alpha_e, label = cultural))+
-    theme_bw()
+  geom_raster(aes(x = alpha_g, y = alpha_e, fill = cultural)) +
+  scale_fill_gradientn(colors=c("red","white","blue")) +
+  geom_text(aes(x = alpha_g, y = alpha_e, label = cultural))+
+  theme_bw()
 
 # HISTOGRAMS:
 test_mean <- read_csv("output_test/test_many.csv") %>%
@@ -138,7 +161,7 @@ test_mean <- read_csv("output_test/test_many.csv") %>%
 test_many <- read_csv("output_test/test_many.csv") %>%
   full_join(test_mean)
 ggplot(data=test_many) + 
-# geom_histogram(aes(x = n, fill=cultural), color="black") +
+  # geom_histogram(aes(x = n, fill=cultural), color="black") +
   geom_density(aes(x = n, fill=cultural)) +
   geom_vline(xintercept = 39, linetype="dashed") +
   scale_fill_gradientn(colors=c("blue","white","red"), name="Number of\n'cultural' traits") +
@@ -146,16 +169,13 @@ ggplot(data=test_many) +
   theme_bw()
 
 ########################################################
-
-
-# SIMS FOR MS:
-
 # FIGURE 2
 tic()
 test <- run_oranzees(t_max = 6000, alpha_e = 0.8, alpha_g = 0.2, S = 1, init_world = TRUE, n_run = 20)
 toc()
 write_csv(test, "sims_for_ms/data/figure_2.csv")
 
+########################################################
 # FIGURE 3
 tic()
 S = seq(0, 1, by = 0.1)
