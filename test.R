bfd01a4da3cfdf38a2a100713aa35af404764e75
Alberto Acerbi <alberto.acerbi@gmail.com>
2019-10-01 16:07:42 +0100

    added mention of parameter S

--- test.R
+++ test.R
@@ -51,73 +51,77 @@ analyse_patterns <- function(my_test){
   output
 }
 
+########################################################
+########################################################
 
-# # TEST 1
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 0, alpha_g = 0, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_0_0.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 0, alpha_g = 0.5, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_0_05.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 0, alpha_g = 1, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_0_1.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 0.5, alpha_g = 0, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_05_0.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 0.5, alpha_g = 0.5, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_05_05.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 0.5, alpha_g = 1, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_05_1.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 1, alpha_g = 0, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_1_0.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 1, alpha_g = 0.5, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_1_05.csv")
-# 
-# tic()
-# test <- run_oranzees(t_max = 6000, alpha_e = 1, alpha_g = 1, init_world = TRUE, n_run = 20)
-# toc()
-# write_csv(test, "output_test/test_1_1.csv")
-# 
-# 
-# # TEST 2
-# 
-# alpha_e_test = seq(0.5, 1, by = 0.1)
-# alpha_g_test = seq(0, 0.5, by = 0.1)
-# 
-# output <- tibble(alpha_e = rep(alpha_e_test, each = 30), 
-#                  alpha_g = rep(rep(alpha_g_test, each = 5), 6),
-#                  n = rep(NA, 180))
-# 
-# for(e in alpha_e_test){
-#   for(g in alpha_g_test) {
-#     test <- run_oranzees(t_max = 6000, alpha_e = e, alpha_g = g, init_world = TRUE, n_run = 5)
-#     out <- analyse_patterns(test)
-#     output[output$alpha_e == e & output$alpha_g == g, ]$n <- out$n[16:20]
-#     print(e)
-#   }
-# }
-# write_csv(output, "output_test/test_many.csv")
+# TESTS AFTER DEVELOPMENT:
+
+# TEST 1
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 0, alpha_g = 0, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_0_0.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 0, alpha_g = 0.5, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_0_05.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 0, alpha_g = 1, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_0_1.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 0.5, alpha_g = 0, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_05_0.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 0.5, alpha_g = 0.5, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_05_05.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 0.5, alpha_g = 1, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_05_1.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 1, alpha_g = 0, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_1_0.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 1, alpha_g = 0.5, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_1_05.csv")
+
+tic()
+test <- run_oranzees(t_max = 6000, alpha_e = 1, alpha_g = 1, init_world = TRUE, n_run = 20)
+toc()
+write_csv(test, "output_test/test_1_1.csv")
+
+
+# TEST 2
+
+alpha_e_test = seq(0.5, 1, by = 0.1)
+alpha_g_test = seq(0, 0.5, by = 0.1)
+
+output <- tibble(alpha_e = rep(alpha_e_test, each = 30),
+                 alpha_g = rep(rep(alpha_g_test, each = 5), 6),
+                 n = rep(NA, 180))
+
+for(e in alpha_e_test){
+  for(g in alpha_g_test) {
+    test <- run_oranzees(t_max = 6000, alpha_e = e, alpha_g = g, init_world = TRUE, n_run = 5)
+    out <- analyse_patterns(test)
+    output[output$alpha_e == e & output$alpha_g == g, ]$n <- out$n[16:20]
+    print(e)
+  }
+}
+write_csv(output, "output_test/test_many.csv")
 
 
 ########################################################
@@ -143,7 +147,7 @@ for(e in alpha_e_test){
 }
 write_csv(output, "sims_for_ms/data/figure_1.csv")
 
-# TWO POSSIBLE VISUALISATIONS:
+# THREE POSSIBLE VISUALISATIONS:
 # MATRIX:
 test_many <- read_csv("output_test/test_many.csv") %>%
   group_by(alpha_g, alpha_e) %>%
@@ -154,19 +158,21 @@ ggplot(data=test_many) +
   geom_text(aes(x = alpha_g, y = alpha_e, label = cultural))+
   theme_bw()
 
-# HISTOGRAMS:
+# HISTOGRAMS (if line 168 is uncommented) OR DENSITY (if line 169 is uncommented):
 test_mean <- read_csv("sims_for_ms/data/figure_1.csv") %>%
   group_by(alpha_g, alpha_e) %>%
   summarise( cultural = mean(n))
 test_many <- read_csv("sims_for_ms/data/figure_1.csv") %>%
   full_join(test_mean)
 ggplot(data=test_many) + 
-  # geom_histogram(aes(x = n, fill=cultural), color="black") +
-  geom_density(aes(x = n, fill=cultural)) +
+  geom_histogram(aes(x = n, fill=cultural), color="black") +
+  # geom_density(aes(x = n, fill=cultural)) +
   geom_vline(xintercept = 39, linetype="dashed") +
   scale_fill_gradientn(colors=c("blue","white","red"), name="Number of\n'cultural' traits") +
   facet_grid(alpha_e ~ alpha_g, labeller=label_both) +
   theme_bw()
+
+
 
 ########################################################
 # FIGURE 2
